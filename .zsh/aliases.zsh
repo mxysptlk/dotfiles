40c6c41f5280f448828373e22dd652d308bf860d
William Smith <pastorofmuppets@me.com>
2024-02-05 10:19:19 -0800

    changed wg aliases

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -1,4 +1,7 @@
-WG_CONF=~/.config/wireguard/wg0.conf
-alias wgup="nmcli connection import type wireguard file $WG_CONF"
-alias wgdown="nmcli connection delete wg0"
-alias config='git --git-dir=/home/williams/.cfg/ --work-tree=/home/williams'
+
+if [[ "$OS" == Linux ]]; then
+    alias wgup="nmcli connection up wg0"
+    alias wgdown="nmcli connection down wg0"
+fi
+alias ping="ping -c 5"
+alias config="git --git-dir=/home/williams/.cfg/ --work-tree=$HOME"
