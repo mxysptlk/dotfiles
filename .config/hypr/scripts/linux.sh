7f29fe11fd838a72f8c5802871eb6ca8e8822cc7
William Smith <pastorofmuppets@me.com>
2024-02-05 12:55:33 -0800

    more hypr config

--- .config/hypr/scripts/linux.sh
+++ .config/hypr/scripts/linux.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-
+export XDG_DATA_DIRS=/home/williams/.local/share/flatpak/exports/share:/var/lib/flatpak/exports/share:/usr/local/share/:/usr/share/
+/usr/libexec/polkit-agent-helper-1 &
