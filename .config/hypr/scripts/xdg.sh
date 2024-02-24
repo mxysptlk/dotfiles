1e09d2773445b9548ff474d1c77734832865521d
William Smith <pastorofmuppets@me.com>
2024-02-24 14:00:41 -0800

    hyperland update

--- .config/hypr/scripts/xdg.sh
+++ .config/hypr/scripts/xdg.sh
@@ -11,12 +11,7 @@ killall xdg-desktop-portal-hyprland
 killall xdg-desktop-portal-wlr
 killall xdg-desktop-portal
 
-if [ -f $PREFIX/xdg-desktop-portal-hyprland ]
-then
-    $PREFIX/xdg-desktop-portal-hyprland &
-else
-    $PREFIX/xdg-desktop-portal-wlr &
-fi
+$PREFIX/xdg-desktop-portal-hyprland &
 
 sleep 2
 $PREFIX/xdg-desktop-portal &
