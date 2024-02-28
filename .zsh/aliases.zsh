2ea4e669c715c6b666c08da26979e5e2d9a811c5
William Smith <pastorofmuppets@me.com>
2024-02-28 13:57:38 -0800

    added aliases for eza

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -3,5 +3,15 @@ if [[ "$OS" == Linux ]]; then
     alias wgup="nmcli connection up wg0"
     alias wgdown="nmcli connection down wg0"
 fi
+
+if [ $(which eza) ]; then
+    alias ls="eza --icons=auto"
+    alias ll="ls -lhg --git"
+    alias la="ll -a"
+    alias l1="ls -1"
+    alias lls="ll --total-size --git"
+    alias lsd="eza -D1"
+fi
 alias ping="ping -c 5"
 alias config="git --git-dir=/home/williams/.cfg/ --work-tree=$HOME"
+
